9014c66208f1740946c53ceea711984fb473462b
opensource-joe <joe.castle@sas.com>
2023-02-06 05:59:32 -0500

    updates to ch8

--- ch8-functions.py
+++ ch8-functions.py
@@ -219,6 +219,41 @@
 
 # passing an arbitrary number of arguments
 
+# def make_pizza(*toppings):
+#     """Print the list of toppings that have been requested"""
+#     print(toppings)
+    
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+# --------
+
+# loop through arguments
+
+# def make_pizza(*toppings):
+#     """Print the list of toppings that have been requested"""
+#     print(f"Making a pizza with the following toppings: ")
+#     for topping in toppings:
+#         print(f"{topping}")
+    
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+# --------
+
+# mixing positional and arbitrary arguments
+# def make_pizza(size, *toppings):
+#     """Summarize the pizza we are about to make"""
+#     print(f"Making a {size}-inch pizza with the following toppings: ")
+#     for topping in toppings:
+#         print(f"{topping}")
+    
+# make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
+
+# --------
+
+# using arbitrary keyword arguments
+
+
 
 
 # -------------------------------
