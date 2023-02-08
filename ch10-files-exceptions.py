16217bb8c1c03cb582289f319f4e1a94cd6423c1
opensource-joe <joe.castle@sas.com>
2023-02-08 10:10:16 -0500

    adding ch10 additional files

--- ch10-files-exceptions.py
+++ ch10-files-exceptions.py
@@ -1,9 +1,6 @@
 # import statements
 # pathlib import to read contents from a file
-# from pathlib import Path
-
-# pathlib import to read contents from a large file
-# from pathlib import Path
+from pathlib import Path
 
 # -------------------------------
 
@@ -88,19 +85,72 @@
 # --------
 
 # using exceptions to handle crashes
-print("Give me two numbers and I'll dived them.")
-print("Enter 'q' to quit.")
+# print("Give me two numbers and I'll dived them.")
+# print("Enter 'q' to quit.")
 
-while True:
-    first_number = input("First number: ")
-    if first_number == 'q':
-        break
-    second_number = input("Second number: ")
-    if second_number == 'q':
-        break
-    answer = int(first_number) / int(second_number)
-    print(answer)
+# while True:
+#     first_number = input("First number: ")
+#     if first_number == 'q':
+#         break
+#     second_number = input("Second number: ")
+#     if second_number == 'q':
+#         break
+#     answer = int(first_number) / int(second_number)
+#     print(answer)
 
+# --------
+
+# the else block
+# print("Give me two numbers and I'll dived them.")
+# print("Enter 'q' to quit.")
+
+# while True:
+#     first_number = input("First number: ")
+#     if first_number == 'q':
+#         break
+#     second_number = input("Second number: ")
+#     if second_number == 'q':
+#         break
+#     try:
+#         answer = int(first_number) / int(second_number)
+#     except ZeroDivisionError:
+#         print("You can't divide by 0!")
+#     else:
+#         print(answer)
+
+# --------
+
+# handling the FileNotFoundError exception
+# path = Path('ch10-files/alice.txt')
+
+# try:
+#     contents = path.read_text(encoding='utf-8')
+# except FileNotFoundError:
+#     print(f"Sorry, the file {path} does not exist.")
+
+# --------
+
+# analyzing text
+# path = Path('ch10-files/alice_new.txt')
+# try:
+#     contents = path.read_text(encoding='utf-8')
+# except FileNotFoundError:
+#     print(f"Sorry, the file {path} does not exist.")
+# else:
+#     # count the appropriate number of words in the file
+#     words = contents.split()
+#     num_words = len(words)
+#     print(f"The file {path} has about {num_words} words.")
+
+# --------
+
+# working with multiple files
+# look at word_count_multiple_texts.py and alice_new.txt
+
+# --------
+
+# failing silently
+#look at word_count_failing_silently.py and alice_new.txt
 
 
 # -------------------------------
@@ -177,3 +227,29 @@ while True:
 #     file_string += f"{name}\n"
 
 # path.write_text(file_string)
+
+# 10-6. Addition: One common problem when prompting for numerical input occurs when people provide text instead of numbers. When you try to convert the input to an int, you’ll get a ValueError. Write a program that prompts for two numbers. Add them together and print the result. Catch the ValueError if either input value is not a number, and print a friendly error message. Test your program by entering two numbers and then by entering some text instead of a number.
+
+
+
+
+
+# 10-7. Addition Calculator: Wrap your code from Exercise 10-5 in a while loop so the user can continue entering numbers, even if they make a mistake and enter text instead of a number.
+
+# 10-8. Cats and Dogs: Make two files, cats.txt and dogs.txt. Store at least three names of cats in the first file and three names of dogs in the second file. Write a program that tries to read these files and print the contents of the file to the screen. Wrap your code in a try-except block to catch the FileNotFound error, and print a friendly message if a file is missing. Move one of the files to a different location on your system, and make sure the code in the except block executes properly.
+
+# 10-9. Silent Cats and Dogs: Modify your except block in Exercise 10-7 to fail silently if either file is missing.
+
+# 10-10. Common Words: Visit Project Gutenberg (https://gutenberg.org) and find a few texts you’d like to analyze. Download the text files for these works, or copy the raw text from your browser into a text file on your computer.
+
+# You can use the count() method to find out how many times a word or phrase appears in a string. For example, the following code counts the number of times 'row' appears in a string:
+
+# >>> line = "Row, row, row your boat"
+# >>> line.count('row')
+# 2
+# >>> line.lower().count('row')
+# 3
+
+# Notice that converting the string to lowercase using lower() catches all appearances of the word you’re looking for, regardless of how it’s formatted.
+
+# Write a program that reads the files you found at Project Gutenberg and determines how many times the word 'the' appears in each text. This will be an approximation because it will also count words such as 'then' and 'there'. Try counting 'the ', with a space in the string, and see how much lower your count is.
---
+++ ch10-files/word_count_multiple_texts.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+def count_words(path):
+    """Count the approximate number of words in a file."""
+    try:
+        contents = path.read_text(encoding='utf-8')
+    except FileNotFoundError:
+        print(f"Sorry, the file {path} does not exist.")
+    else:
+        # count the appropriate number of words in the file
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {path} has about {num_words} words.")
+
+filenames = ['alice_new.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+for filename in filenames:
+    path = Path('ch10-files/', filename)
+    count_words(path)
